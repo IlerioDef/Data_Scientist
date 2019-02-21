e730bcf4d5132afda0fdd59e532c0d6b3317f4ea
Ilerio Def <47086473+IlerioDef@users.noreply.github.com>
2019-02-21 23:17:58 +0400

    Update hw2.sql

--- hw2.sql
+++ hw2.sql
@@ -3,17 +3,17 @@ Select 'ФИО: Шумков Никита Олегович, дз2';
 Select * from ratings
 limit 10;
 --1.2 WHERE, LIKE - выбрать из таблицы links всё записи, у которых imdbid оканчивается на "42", а поле movieid между 100 и 1000
-(select * from links where imdbid like '%42') union all (select *from links where movieid >100) 
+(select * from links where imdbid like '%42') INTERSECT (select *from links where movieid >100) 
 INTERSECT 
 (select * from links where movieid <1000)
-limit 10; 
+limit 30; 
 --2.1 INNER JOIN выбрать из таблицы links все imdbId, которым ставили рейтинг 5
 select imdbid from links 
 inner join ratings 
 on links.movieid=ratings.movieid where rating=5 limit 10; 
 --3.1 COUNT() Посчитать число фильмов без оценок
-COUNT(imdbdid) from links
-inner join ratings on
+COUNT (imdbdid) from links
+outer join ratings on
 links.movieid=ratings.movieid
  where rating is null limit 10; -- проверил select * from ratings where rating is null limit 10, показал, что пустых значений нет, значит фильмов без оценок нет. Вроде бы.
 --3.2 GROUP BY, HAVING вывести top-10 пользователей, у который средний рейтинг выше 3.5
@@ -27,43 +27,9 @@ HAVING AVG(rating) > 3.5
 ORDER BY Rate_Numbers Desc
 LIMIT 10;
 --4.1 Подзапросы: достать любые 10 imbdId из links у которых средний рейтинг больше 3.5.
-select Imdbid from links 
-right join 
-on links.movieid=ratings.movieid  
+SELECT imdbid, rating
+FROM links
+INNER JOIN ratings
+ON links.movieid=ratings.movieid
+WHERE rating > (SELECT AVG(rating) FROM public.ratings HAVING AVG(rating) >3.5) limit 10;
 
-
-SELECT
-    links.movieid,
-    COUNT(rating) as Number_of_rates
-    FROM ratings
-    right join links on links.movieid=ratings.movieid
-GROUP BY ratings.movieid
-HAVING ratings.rating is null
-ORDER BY ratings.movieid
-LIMIT 10;
-
-SELECT
-    COUNT(userId) as count,
-    COUNT(DISTINCT userId) as count_distinct,
-    COUNT(DISTINCT userId)/CAST(COUNT(userId) as float) unique_fraction
-FROM public.ratings;
-
-
-SELECT *
-FROM public.links
-LEFT JOIN public.ratings
-    ON links.movieid=ratings.movieid
-WHERE Count(ratings.movieid) IS NULL
-LIMIT 5;
-
-
-WITH tmp_table
-AS (
-    SELECT *
-    FROM ratings
-    left join links on links.movieid=ratings.movied)
-SELECT
-    userId,
-    COUNT(movieid) having rating is null
-GROUP BY movieid
-LIMIT 10;
